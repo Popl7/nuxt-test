eb079dea61a24f292487e0d73c84b02b14b81b5c
Steven Thonus <steven.thonus@infi.nl>
2018-07-22 12:31:38 +0200

    initial nuxt setup

---
+++ pages/counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        counter
+      </h1>
+    </div>
+    <div>
+      <h3>{{ $store.state.counter }}</h3>
+      <button @click="$store.commit('increment')">increment</button>
+      <button @click="$store.commit('decrement')">decrement</button>
+    </div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        home
+      </h1>
+    </div>
+    <div>
+      <h3>{{ $store.state.counter }}</h3>
+    </div>
+  </section>
+</template>
+
+<script>
+// import AppLogo from '~/components/AppLogo.vue'
+
+// export default {
+//   components: {
+//     AppLogo
+//   }
+// }
+</script>
+
+<style>
+</style>
+
